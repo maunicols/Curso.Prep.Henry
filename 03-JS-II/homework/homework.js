bab81f9d75168acdcf6b0e7dbf53f0b8e7d07ede
maunicols <crusmauricionicolas@gmail.com>
2020-06-30 11:26:26 -0300

    intento de subir mi homework resuelto

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,10 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x > y) {
+    return x;
+  }
+  return y;
 }
 
 function saludo(idioma) {
@@ -14,18 +18,30 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma==='aleman') {
+    return 'Guten Tag!';
+  } else if (idioma==='mandarin'){
+    return 'Ni Hao!';
+  } else if (idioma==='ingles'){
+    return 'Hello!';
+} else {
+  return 'Hola!';
+}
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    return (numero===10 || numero===5)
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (50>numero && numero>20)
+
 }
 
 function esEntero(numero) {
@@ -36,6 +52,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return (Math.floor(numero)===numero)
 }
 
 function fizzBuzz(numero) {
@@ -43,6 +60,16 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  //((Tu código)(:3))
+  if (numero%15===0){
+    return 'fizzbuzz';
+  } 
+  else if (numero%5===0){
+    return 'buzz';
+  } else if (numero%3===0){
+    return 'fizz';
+  } else {return numero; }
+
 }
 
 function esPrimo(numero) {
@@ -51,8 +78,19 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+  // Tu código:
+  if (numero<2) {return false;}
+  if (numero===2) {return true;}
+  for (var i= 2;i<numero; i++)
+    if (numero%i===0) {return false;
+    }
+    {
+      return true;
+    }
 
+}
+//Terminé encontrando que la solucion propuesta en elrepositorio de henry es la traducción de el test de Fermat. 
+//El cual no conocía pero tuve que investigar para saber que lo que hacia tiene sentido.
 
 
 // No modificar nada debajo de esta línea
